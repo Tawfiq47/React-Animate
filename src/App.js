b4a90638a6a5c6a83fe90d046ae5a8d0b6772efa
Toufik khan <toufik9909@gmail.com>
2023-08-08 11:02:35 +0600

    react-animate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { CSSTransition } from 'react-transition-group';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    show: true
+  }
+  render() { 
+
+    return (
+      <div className="App">
+        <button onClick={() => this.setState({ show: !this.state.show })}>toggle</button>
+
+        <CSSTransition in={this.state.show} timeout={500} mountOnEnter unmountOnExit classNames="myClass">
+          {
+            mode =>(
+              <div style={{
+                background: 'black',
+                color: 'white',
+                borderRadius: 5
+              }}>
+                <p>i am some text</p>
+              </div>
+              )
+          }
+        </CSSTransition>  
+        <p>i am outside transition</p>
+      </div>
+    );
+  }
+
 }
 
 export default App;
